545c07e0e13a2c8189e35efebd7f3e27172eddfa
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-10-15 20:01:30 +0530

    Typescript

---
+++ one.js
@@ -0,0 +1,305 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var a = 4;
+// any code that is a valid js code is a valid ts code
+var arr = [1, 2, 4, 5];
+var arr2 = arr;
+console.log(arr2.pop());
+console.log(arr);
+console.log(arr2);
+var b;
+//  If you are just defining a variable then you need to specify it's type.
+var c = 2;
+//  But if you are declaring it then it's no need!
+var g = 5;
+console.log(typeof g);
+//  types in ts
+var aa = 0;
+var bb = "";
+var tt = true;
+var array = [1, 2, 3, 4];
+var hsd = ["vikram", "nitin"];
+//  tuples
+//  with formation
+var tup = [
+    23,
+    "Vikram",
+    "nitin",
+    "bhappu",
+    true,
+];
+console.log(tup);
+//  without formation
+var tupl = ['ldfkjslfj', false, 343, 324, 'lkfjsldfjsdljf', true];
+// The spread operator (...) is used in TypeScript to indicate a rest element in a tuple, but it can only be applied once and must be at the end of the tuple definition. You can't spread multiple types like this within a tuple.
+// const tup: [...number[], ...string[], ...boolean[]] = [23, 42, 'Vikram', 'John', true, false];
+// How to Correct It
+// You can achieve a similar effect using union types or a rest element at the end:
+// what is a rest element?
+// const [first, ...rest] = [1, 2, 3, 4, 5];
+// 1. Using Union Types in an Array
+// If you want a flexible array with numbers, strings, and booleans, you can define it as an array of union types:
+// const arr: (number | string | boolean)[] = [23, 42, 'Vikram', 'John', true, false];
+// 2. Using a Tuple with a Rest Element
+// If you want to define a tuple with a specific order followed by a rest of mixed types, you can use a rest element at the end of the tuple definition:
+// const tup: [number, ...string[], ...boolean[]] = [23, 'Vikram', 'John', true, false];
+// --------------------------------------------------------------------------------------------------
+//  any, unknown, never, void
+//  unknown and any works very similar - we use unknown when we are not sure about what type do we want our variable or fn to be...and any is like make it any type
+//  the use of never is very trick - it basically says that 'if used on a fn it will never return anything and the code after that will not run either'
+function neverReturnAnythin() {
+    while (true)
+        console.log("infinit run");
+}
+// neverReturnAnythin()
+console.log("hey");
+function returnNumber() {
+    return 12;
+}
+returnNumber();
+function returnsNothing() {
+    console.log("hey");
+}
+// -----------------------ENUMS---------------------
+// let currentDirection: string = 'north'
+var Directions;
+(function (Directions) {
+    Directions[Directions["east"] = 0] = "east";
+    Directions[Directions["west"] = 1] = "west";
+    Directions[Directions["north"] = 2] = "north";
+    Directions[Directions["south"] = 3] = "south";
+})(Directions || (Directions = {}));
+var currentDirection = Directions.north;
+console.log(currentDirection);
+var currentDirection2 = Directions.east; // if you assign it to string - it won't work as the default indexes are numbers
+var Roles;
+(function (Roles) {
+    Roles["admin"] = "ADMIN";
+    Roles["user"] = "USER";
+})(Roles || (Roles = {}));
+function userRole(role) {
+    if (role === Roles.admin)
+        return "You are admin!";
+    return "Regular User";
+}
+// function userRole(role: Roles){
+//     if(role === Roles.admin) return console.log("You are admin!");
+//     console.log("Regular User");
+// }
+console.log(userRole(Roles.user));
+// -------------------------------------UNION TYPES-------------------------------------------------------
+var value;
+value = 32;
+if (typeof value === "string") {
+    // console.log(value.length);
+}
+else if (typeof value === "number") {
+    console.log(value.toFixed());
+}
+//  abhi in mese koi ek chal raha he but yeh same code chalega age isee hum ek function me likh dinge toh
+function diffTypes(val) {
+    if (typeof val === "string") {
+        console.log(val.length);
+    }
+    else if (typeof val === "number") {
+        console.log(val.toFixed());
+    }
+}
+diffTypes(23);
+diffTypes("heyyy");
+//  see it works like a charm
+// -------------------------------------INTERSECTION TYPES-------------------------------------------------------
+var Section;
+(function (Section) {
+    Section["A"] = "A";
+    Section["B"] = "B";
+})(Section || (Section = {}));
+var student = [
+    {
+        section: Section.A,
+        name: "Vikram",
+    },
+    {
+        section: Section.B,
+        name: "Falcon",
+    },
+];
+console.log(student);
+var firstName = '';
+var human = {
+    name: "Dychord",
+    age: 23,
+    email: "iamdychord@gmail.com"
+};
+console.log(human);
+function getUserDetails(user) {
+    user.name = 'Dychord';
+    user.password = 'sldkfjsldkjflfl2k3j2lkjr34534lkj';
+}
+function getStudentDetails(student) {
+    student.name = 'Vikram',
+        student.gender = 'Male';
+}
+// ---------------------------CLASS----------------------------
+var hghg = 'hello';
+var AirConditioner = /** @class */ (function () {
+    function AirConditioner() {
+        this.temp = 16;
+    }
+    AirConditioner.prototype.turnOnAC = function (name) {
+        console.log("Turned On ".concat(name));
+    };
+    AirConditioner.prototype.raiseTemp = function () {
+        console.log(this.temp++);
+    };
+    AirConditioner.prototype.turnOffAC = function (name) {
+        console.log("Turned Off ".concat(name));
+    };
+    return AirConditioner;
+}());
+var ac = new AirConditioner();
+ac.turnOnAC('Ac1');
+//  CONSTRUCTORS - In TypeScript, constructors are special methods within classes that are responsible for initializing the class's properties when an object of the class is created. The constructor is called automatically when a new instance of the class is created using the new keyword.
+var ClassName = /** @class */ (function () {
+    function ClassName() {
+        // Initialization code
+    }
+    return ClassName;
+}());
+var Car = /** @class */ (function () {
+    function Car(name, discount) {
+        this.name = name,
+            this.discount = discount;
+    }
+    Car.prototype.carDetails = function () {
+        console.log(this.name, this.discount);
+    };
+    return Car;
+}());
+var car = new Car('Tesla', 120000);
+car.carDetails();
+//  **ACCESS MODIFIER BY DEFUALT IS **PUBLIC**
+var Games = /** @class */ (function () {
+    function Games(name) {
+    }
+    return Games;
+}());
+//  instead of this - we could also do this
+var Games1 = /** @class */ (function () {
+    function Games1(name) {
+        this.name = name;
+    }
+    return Games1;
+}());
+// -------------PUBLIC
+var Bike = /** @class */ (function () {
+    function Bike() {
+        this.name = 'Ducatti';
+    }
+    Bike.prototype.getBikeName = function () {
+        console.log(this.name);
+    };
+    return Bike;
+}());
+var bike = new Bike();
+bike.name = 'YAMAHA'; // we can later change this bcz of public keyword
+// bike.getBikeName()
+// --------------PRIVATE
+var Bike2 = /** @class */ (function () {
+    function Bike2() {
+        this.name = 'Ducatti';
+    }
+    Bike2.prototype.getBikeName = function () {
+        console.log(this.name);
+    };
+    Bike2.prototype.setBikeName = function (name) {
+        this.name = name;
+    };
+    return Bike2;
+}());
+var bike2 = new Bike2(); // we can later change this bcz of public keyword
+bike2.setBikeName("YAMAHA");
+bike2.getBikeName();
+// --------------PROTECTED
+var People = /** @class */ (function () {
+    function People() {
+        this.limbs = 4; // can be accessed by same and the class that extends it
+        this.height = 6.1; // only be accessable by this same class
+    }
+    return People;
+}());
+var Person = /** @class */ (function (_super) {
+    __extends(Person, _super);
+    function Person() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Person.prototype.getLimbs = function () {
+        console.log(this.limbs);
+    };
+    Person.prototype.setlimbs = function (limbs) {
+        this.limbs = limbs;
+    };
+    return Person;
+}(People));
+var person = new Person();
+person.eye = 4; // only a variable or fn with public keyword can be manipulated outside of it's class scope or child class scope
+person.setlimbs(6);
+person.getLimbs();
+// --------------------READONLY and GETTER SETTER-------------------
+var Laptops = /** @class */ (function () {
+    function Laptops(discount) {
+        this.discount = discount;
+        this.discount = discount;
+    }
+    Object.defineProperty(Laptops.prototype, "name", {
+        //  predefined getter and setter
+        get: function () {
+            return this.username;
+        },
+        set: function (username) {
+            this.username = username;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    //  how i like
+    // getDiscount() {
+    //     console.log(`The discount of the product is fixed ${this.discount}`);
+    // }
+    Laptops.prototype.getDiscount = function () {
+        return this.discount;
+    };
+    Laptops.prototype.setDiscount = function (discount) {
+        // this.discount = discount
+    };
+    return Laptops;
+}());
+var lap = new Laptops(8);
+lap.name = 'Dychord';
+console.log(lap.name);
+console.log(lap.getDiscount());
+lap.setDiscount(10);
+lap.name = 'Lenovo Ideapad Slim i5';
+console.log(lap.name);
+// ------------------------------FUNCTIONS-------------------------------
+// Named Function
+// Anonymus Function
+// Arrow Function
+// Explicit Return Type
+// Implicit Return Type
+// Optional Parameters
+// Default Parameters
+// Rest Parameters
---
+++ one.ts
@@ -0,0 +1,407 @@
+let a = 4;
+// any code that is a valid js code is a valid ts code
+let arr = [1, 2, 4, 5];
+let arr2 = arr;
+console.log(arr2.pop());
+console.log(arr);
+console.log(arr2);
+
+let b: number;
+//  If you are just defining a variable then you need to specify it's type.
+let c = 2;
+//  But if you are declaring it then it's no need!
+
+let g: number = 5;
+console.log(typeof g);
+
+//  types in ts
+const aa: number = 0;
+const bb: string = "";
+const tt: boolean = true;
+const array: number[] = [1, 2, 3, 4];
+const hsd: string[] = ["vikram", "nitin"];
+
+
+//  tuples
+//  with formation
+const tup: [number, ...string[], boolean] = [
+    23,
+    "Vikram",
+    "nitin",
+    "bhappu",
+    true,
+];
+console.log(tup);
+
+//  without formation
+const tupl: (string | number | boolean)[] = ['ldfkjslfj', false, 343, 324, 'lkfjsldfjsdljf', true]
+
+// The spread operator (...) is used in TypeScript to indicate a rest element in a tuple, but it can only be applied once and must be at the end of the tuple definition. You can't spread multiple types like this within a tuple.
+// const tup: [...number[], ...string[], ...boolean[]] = [23, 42, 'Vikram', 'John', true, false];
+
+// How to Correct It
+// You can achieve a similar effect using union types or a rest element at the end:
+// what is a rest element?
+// const [first, ...rest] = [1, 2, 3, 4, 5];
+
+// 1. Using Union Types in an Array
+// If you want a flexible array with numbers, strings, and booleans, you can define it as an array of union types:
+
+// const arr: (number | string | boolean)[] = [23, 42, 'Vikram', 'John', true, false];
+
+// 2. Using a Tuple with a Rest Element
+// If you want to define a tuple with a specific order followed by a rest of mixed types, you can use a rest element at the end of the tuple definition:
+
+// const tup: [number, ...string[], ...boolean[]] = [23, 'Vikram', 'John', true, false];
+
+// --------------------------------------------------------------------------------------------------
+//  any, unknown, never, void
+//  unknown and any works very similar - we use unknown when we are not sure about what type do we want our variable or fn to be...and any is like make it any type
+
+//  the use of never is very trick - it basically says that 'if used on a fn it will never return anything and the code after that will not run either'
+function neverReturnAnythin(): never {
+    while (true) console.log("infinit run");
+}
+
+// neverReturnAnythin()
+console.log("hey");
+
+function returnNumber(): number {
+    return 12;
+}
+returnNumber();
+
+function returnsNothing(): void {
+    console.log("hey");
+}
+
+// -----------------------ENUMS---------------------
+
+// let currentDirection: string = 'north'
+enum Directions {
+    east, // 0
+    west, // 1
+    north, // 2
+    south, // 3
+}
+let currentDirection: Directions = Directions.north;
+console.log(currentDirection);
+
+let currentDirection2: number = Directions.east; // if you assign it to string - it won't work as the default indexes are numbers
+
+enum Roles {
+    admin = "ADMIN",
+    user = "USER",
+}
+function userRole(role: Roles): string {
+    if (role === Roles.admin) return "You are admin!";
+    return "Regular User";
+}
+// function userRole(role: Roles){
+//     if(role === Roles.admin) return console.log("You are admin!");
+//     console.log("Regular User");
+// }
+console.log(userRole(Roles.user));
+
+// -------------------------------------UNION TYPES-------------------------------------------------------
+
+let value: string | number;
+value = 32;
+
+if (typeof value === "string") {
+    // console.log(value.length);
+} else if (typeof value === "number") {
+    console.log(value.toFixed());
+}
+
+//  abhi in mese koi ek chal raha he but yeh same code chalega age isee hum ek function me likh dinge toh
+
+function diffTypes(val: number | string) {
+    if (typeof val === "string") {
+        console.log(val.length);
+    } else if (typeof val === "number") {
+        console.log(val.toFixed());
+    }
+}
+diffTypes(23);
+diffTypes("heyyy");
+
+//  see it works like a charm
+
+// -------------------------------------INTERSECTION TYPES-------------------------------------------------------
+
+enum Section {
+    "A" = 'A',
+    "B" = 'B'
+}
+
+type Classmates = {
+    section: string;
+};
+type Students = {
+    name: string;
+};
+
+type CS = Classmates & Students;
+
+
+let student: CS[] = [
+    {
+        section: Section.A,
+        name: "Vikram",
+    },
+    {
+        section: Section.B,
+        name: "Falcon",
+    },
+];
+
+console.log(student);
+
+
+
+
+// -------------------------------TYPE ALIASES-----------------------------
+
+// primitive type ALIASES
+type Name = string
+const firstName: Name = '';
+
+// object type aliases
+type Human = {
+    name: string,
+    age: number,
+    email: string
+}
+
+const human: Human = {
+    name: "Dychord",
+    age: 23,
+    email: "iamdychord@gmail.com"
+}
+
+console.log(human);
+
+
+
+// ----------------------INTERFACES---------------------
+// Interface humhe class and objects ka shape banake deta he
+
+interface User {
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    age: number,
+    gender: string
+}
+
+function getUserDetails(user: User) {
+    user.name = 'Dychord'
+    user.password = 'sldkfjsldkjflfl2k3j2lkjr34534lkj'
+}
+
+
+// extends keyword
+
+interface Student extends User {
+
+}
+
+function getStudentDetails(student: Student) {
+    student.name = 'Vikram',
+        student.gender = 'Male'
+}
+
+
+// ---------------------------CLASS----------------------------
+
+const hghg: string = 'hello';
+
+class AirConditioner {
+    discount!: number;     // '!' indicates that discount will be definitely assigned later
+    name!: string;
+    temp: number = 16;
+
+    turnOnAC(name: string) {
+        console.log(`Turned On ${name}`);
+    }
+
+    raiseTemp() {
+        console.log(this.temp++);
+    }
+
+    turnOffAC(name: string) {
+        console.log(`Turned Off ${name}`);
+    }
+
+}
+
+const ac = new AirConditioner();
+
+ac.turnOnAC('Ac1')
+
+
+//  CONSTRUCTORS - In TypeScript, constructors are special methods within classes that are responsible for initializing the class's properties when an object of the class is created. The constructor is called automatically when a new instance of the class is created using the new keyword.
+
+class ClassName {
+    constructor() {
+        // Initialization code
+    }
+}
+
+class Car {
+    discount!: number
+    name!: string
+
+    constructor(name: string, discount: number) {
+        this.name = name,
+            this.discount = discount
+    }
+
+    carDetails() {
+        console.log(this.name, this.discount);
+    }
+}
+
+const car = new Car('Tesla', 120000)
+
+car.carDetails();
+
+//  **ACCESS MODIFIER BY DEFUALT IS **PUBLIC**
+
+class Games {
+    name!: string
+    constructor(name: string) {
+
+    }
+
+}
+
+//  instead of this - we could also do this
+
+class Games1 {
+
+    constructor(public name: string) {   // we didn't have it explicitly declare or intilise the name variable above constructor...if we use public, private or protected expiciltly
+
+    }
+
+}
+
+
+// -------------PUBLIC
+
+class Bike {
+    public name: string = 'Ducatti'
+
+    getBikeName() {
+        console.log(this.name);
+    }
+
+}
+
+const bike = new Bike()
+bike.name = 'YAMAHA'    // we can later change this bcz of public keyword
+// bike.getBikeName()
+
+
+// --------------PRIVATE
+
+class Bike2 {
+    private name: string = 'Ducatti'
+
+    getBikeName() {  // getter
+        console.log(this.name);
+    }
+
+    setBikeName(name: string) {   // setter
+        this.name = name
+    }
+
+}
+
+const bike2 = new Bike2()  // we can later change this bcz of public keyword
+bike2.setBikeName("YAMAHA")
+bike2.getBikeName()
+
+
+// --------------PROTECTED
+
+class People {
+    protected limbs: number = 4;    // can be accessed by same and the class that extends it
+    private height: number = 6.1;    // only be accessable by this same class
+    public eye!: number;
+}
+
+class Person extends People {
+
+    getLimbs() {
+        console.log(this.limbs);
+    }
+    setlimbs(limbs: number) {
+        this.limbs = limbs
+    }
+
+}
+
+const person = new Person()
+person.eye = 4;     // only a variable or fn with public keyword can be manipulated outside of it's class scope or child class scope
+person.setlimbs(6)
+person.getLimbs()
+
+
+
+// --------------------READONLY and GETTER SETTER-------------------
+
+class Laptops {
+
+    username!: string;
+
+    //  predefined getter and setter
+    get name() {
+        return this.username
+    }
+    set name(username: string) {
+        this.username = username
+    }
+
+
+    constructor(public readonly discount: number) {
+        this.discount = discount
+    }
+    //  how i like
+    // getDiscount() {
+    //     console.log(`The discount of the product is fixed ${this.discount}`);
+    // }
+
+    getDiscount() {
+        return this.discount
+    }
+
+    setDiscount(discount: number) {
+        // this.discount = discount
+    }
+
+
+}
+
+const lap = new Laptops(8)
+lap.name = 'Dychord'
+console.log(lap.name);
+console.log(lap.getDiscount());
+lap.setDiscount(10)
+lap.name = 'Lenovo Ideapad Slim i5'
+console.log(lap.name);
+
+
+
+// ------------------------------FUNCTIONS-------------------------------
+
+// Named Function
+// Anonymus Function
+// Arrow Function
+// Explicit Return Type
+// Implicit Return Type
+// Optional Parameters
+// Default Parameters
+// Rest Parameters
